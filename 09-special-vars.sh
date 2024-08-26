19117d5ba3c6a74f75618cc5c214e7bb44b6145d
deepakgajula <gajuladeepak17@gmail.com>
2024-08-26 22:19:19 +0530

    messages

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -1,3 +1,7 @@
 #!bin/bash
 
 echo "All varaibles passed to the script: $@"
+echo "Number of variables passed: $#"
+echo "Script name: $0"
+echo "Home directory of current user: $HOME"
+echo "PID of the current script": $$
