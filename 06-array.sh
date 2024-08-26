b2b75890541c7e9edc98abd4a2b1e741503ed727
deepakgajula <gajuladeepak17@gmail.com>
2024-08-26 19:59:05 +0530

    arrays

---
+++ 06-array.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+FRUITS=("apple","grapes","orange")
+
+echo "First Fruit: ${FRUITS[0]}"
+echo "Second Fruit: ${FRUITS[1]}"
+echo "Third Fruit: ${FRUITS[2]}"
+echo "All Fruits: ${FRUITS[@]}"
