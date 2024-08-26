9b57b69273d827bb386a0f459c6e1b9ade5ba8d2
deepakgajula <gajuladeepak17@gmail.com>
2024-08-26 23:26:31 +0530

    messages

--- 11-installation.sh
+++ 11-installation.sh
@@ -7,12 +7,17 @@ then
     exit 1
 fi
 
-dnf list installed git
-
+dnf list installed gittt
 if [ $? -ne 0 ]
 then
     echo "Git is not installed, going to install it.."
     dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Git installation is not success...check it"
+    else
+        echo "Git installation is success"
+    fi
 else
     echo "Git is already installed, nothing to do.."
 fi
