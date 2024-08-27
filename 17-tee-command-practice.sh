59e9a7fe6bd9e34234b0068e6c415e6c7047e9f4
deepakgajula <gajuladeepak17@gmail.com>
2024-08-27 21:07:17 +0530

    Shell

---
+++ 17-tee-command-practice.sh
@@ -0,0 +1,55 @@
+#!bin/bash
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOGFILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "$R Please run this script with root priveleges $N" | tee -a &>>$LOGFILE
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N" &>>$LOGFILE | tee -a &>>LOGFILE
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N" &>>$LOGFILE | tee -a &>>LOGFILE
+    fi
+}
+USAGE(){
+    echo -e "$R USAGE:: $N sudo sh 16-redirectors.sh package1 package2.."
+    exit 1
+}
+
+echo "Script started executing at: $(date)" &>>LOGFILE | tee -a &>>$LOGFILE
+CHECK_ROOT
+if [ $# -eq 0 ]
+then 
+    USAGE
+fi
+
+for package in $@
+do
+    dnf list installed $package &>>$LOGFILE
+    if [ $? -ne 0 ]
+    then
+        echo "$package is not installed, going to install it.."| tee -a &>>$LOGFILE
+        dnf install $package -y
+        VALIDATE $? "installing $package"
+    else
+        echo -e "$package is already $Y installed..nothing to do $N" | tee - a &>>$LOGFILE
+    fi
+done
