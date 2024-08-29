43eece5b78ea6a8ae434af3feb7adc74294e4005
deepakgajula <gajuladeepak17@gmail.com>
2024-08-29 20:19:44 +0530

    Shell

--- 20-backup.sh
+++ 20-backup.sh
@@ -2,9 +2,14 @@
 
 #i will create a source folder(app-logs) in /home/ec2-user/app-logs
 #i will create a destination folder(backup) in /home/ec2-user/backup
+# before running the script create log files in app-logs
+#cd app-logs/
+#touch 20240101 mysql.log
+
 
 SOURCE_DIR=$1
 DEST_DIR=$2
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 
 R="\e[31m"
 G="\e[32m"
@@ -39,9 +44,28 @@ FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 
 echo "Files: $FILES"
 
-if [ -z $FILES ] #true if files are empty, ! makes it false
+if [ ! -z $FILES ] #true if files are empty, ! makes it false
 then
     echo "Files are found"
+    ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+    find ${SOURCE_DIR} -name "*.log" -mtime +14 | zip "$ZiP_FILE" -@ #here we are zipping all the files returned by find command and file name is ZIP_FILE
+
+    #check if zip is successfully created or not
+    if [ -f $ZiP_FILE ]
+    then
+        echo "Successfully zipped files older than $DAYS"
+
+        #remove the files after zipping
+        while IFS= read -r file #IFS, internal field separator, empty it will ignore white spaces. -r is for not to ignore special characters like /
+        do 
+            echo "Deleting file: $file"
+            rm -rf $file
+
+        done <<< $FILES 
+    else
+        echo "Zipping the files is failed"
+        exit 1
+    fi
 else
     echo "No old files than $DAYS"
 
