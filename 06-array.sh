4d00dff483bed65e77bc3641393e843ce380ad81
deepakgajula <gajuladeepak17@gmail.com>
2025-02-07 21:22:38 +0530

    git practice

--- 06-array.sh
+++ 06-array.sh
@@ -3,5 +3,5 @@ FRUITS=("apple","grapes","orange")
 
 echo "First Fruit: ${FRUITS[0]}"
 echo "Second Fruit: ${FRUITS[1]}"
-echo "Third Fruit: ${FRUITS[2]}"
+echo "Third Fruit: $FRUITS[2]"
 echo "All Fruits: ${FRUITS[@]}"
