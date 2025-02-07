1a5283dc155f0577fc7470374dd0836f7de81154
deepakgajula <gajuladeepak17@gmail.com>
2025-02-07 20:27:38 +0530

    git practice

--- 13-colors.sh
+++ 13-colors.sh
@@ -19,14 +19,14 @@ VALIDATE(){
         echo -e "$2 is... $R FAILED $N"  #$N normal color
         exit 1
     else
-        echo -e "$2 is....$G SUCCESS $N"
+        echo -e "$2 is.... $G SUCCESS $N"
     fi
 }
 
 
 CHECK_ROOT
 
-dnf list installed git -y
+dnf list installed gitttt -y
 
 VALIDATE $? "Listing Git"
 
