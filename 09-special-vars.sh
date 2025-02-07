a50e6103893baa6137cb5ff255c89d4b2a31391f
deepakgajula <gajuladeepak17@gmail.com>
2025-02-07 17:52:02 +0530

    git practice

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -7,5 +7,5 @@ echo "Current Working directory: $PWD"
 echo "Home directory of current user: $HOME"
 echo "PID of the current script": $$
 
-sleep 100
-echo "PID of last background command: $!" #enthaizxi mundhu background lo run chesina command's process instance id
+sleep 100 &
+echo "PID of last background command: $!" #enthaku mundhu background lo run chesina command's process instance id
