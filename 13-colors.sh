65e2454e0a2d251301d736dd8ec590532898dd0d
deepakgajula <gajuladeepak17@gmail.com>
2025-02-07 20:21:12 +0530

    git practice

---
+++ 13-colors.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0"
+
+
+CHECK_ROOT() {
+    if [ $USERID -ne 0 ]
+    then 
+        echo "Root priveleges Requried"
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is... $R FAILED $N"  #$N normal color
+        exit 1
+    else
+        echo -e "$2 is....$G SUCCESS $N"
+    fi
+}
+
+
+CHECK_ROOT
+
+dnf list installed git -y
+
+VALIDATE $? "Listing Git"
+
+if [ $? -ne 0 ]
+then 
+    echo "Git is not installed need to be installed"
+    dnf install git -y
+    VALIDATE $? "Installing git command"
+else
+    echo "Git is already installed"
+fi
