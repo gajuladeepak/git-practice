94bef2661eb572467e7a37e941c5ddd3beea5d3f
deepakgajula <gajuladeepak17@gmail.com>
2025-02-07 20:04:58 +0530

    git practice

--- 12-functions.sh
+++ 12-functions.sh
@@ -4,10 +4,10 @@ USERID=$(id -u)
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "Command is...FAILED"
+        echo "$2 is...FAILED"
         exit 1
     else
-        echo "Command is....SUCCESS"
+        echo "$2 is....SUCCESS"
     fi
 }
 
@@ -21,19 +21,13 @@ fi
 
 dnf list installed git -y
 
-VALIDATE $?
+VALIDATE $? "Listing Git"
 
-# if [ $? -ne 0 ]
-# then 
-#     echo "Git is not installed need to be installed"
-#     dnf install git -y
-#     if [ $? -ne 0 ]
-#     then 
-#         echo "Git installation is failed... Try again"
-#         exit 1
-#     else
-#         echo "Git installation is successfull"
-#     fi
-# else
-#     echo "Git is already installed"
-# fi
+if [ $? -ne 0 ]
+then 
+    echo "Git is not installed need to be installed"
+    dnf install git -y
+    VALIDATE $? "Installing git command"
+else
+    echo "Git is already installed"
+fi
