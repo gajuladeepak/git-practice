10afbcfb492d54eae097292df9c97bd187df3232
deepakgajula <gajuladeepak17@gmail.com>
2025-02-08 12:44:29 +0530

    shell-practice

--- 23-practice.sh
+++ 23-practice.sh
@@ -16,7 +16,7 @@ CHECK_ROOT() {
 
     if [ $USERID -ne 0 ]
     then
-        echo "Please run the cript with user priveleges"
+        echo "$R Please run the cript with user priveleges $N"
 
     fi
 }
