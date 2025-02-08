8ed2636f3722c156d2a9d8f78fe06e46afb27c1e
deepakgajula <gajuladeepak17@gmail.com>
2025-02-08 12:53:35 +0530

    shell-practice

--- 23-practice.sh
+++ 23-practice.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 
-
-
+LOGS_FOLDER="var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOGFILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
 
 
 
@@ -16,28 +19,45 @@ CHECK_ROOT() {
 
     if [ $USERID -ne 0 ]
     then
-        echo -e "$R Please run the cript with user priveleges $N"
+        echo -e "$R Please run the cript with user priveleges $N" | tee -a $LOGFILE
 
     fi
 }
+
+USAGE(){
+    echo "$R USAGE:: $N Expected Inputs sudo sh 16-redirectors.sh package1 package2.."
+}
+
+
 
 VALIDATE() {
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is.... $R FAILED $N"
+        echo -e "$2 is.... $R FAILED $N" | tee -a $LOGFILE
     else
-        echo -e "$2 is... $G SUCCESS $N"
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOGFILE
     fi
 }
 
+echo "Script started executing at: $(date)" &>>LOGFILE | tee -a $LOGFILE
 CHECK_ROOT
+if [ $# -eq 0 ]
+then
+    USAGE
+fi
 
 for package in $@
 do
-    dnf list installed $package -y
-    VALIDATE $? "Installing $package"
-    dnf install $package -y
-    VALIDATE $? "Installing $package"
+    dnf list installed $package -y &>>$LOGFILE
+    if [ $? -ne 0]
+    then 
+        echo echo "$package is not installed, going to install it.."| tee -a $LOGFILE
+        dnf install $package -y
+        VALIDATE $? "Installing $package"
+    else
+         echo -e "$package is already $Y installed..nothing to do $N" | tee -a $LOGFILE
+
+    fi
 
 done
 
