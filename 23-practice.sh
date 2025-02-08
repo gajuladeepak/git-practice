2b1f3b7a0bc93cde6c2b9a2f897a7b8ef6365b14
deepakgajula <gajuladeepak17@gmail.com>
2025-02-08 12:39:59 +0530

    shell-practice

---
+++ 23-practice.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+
+
+
+
+
+USERID=$(id -u)
+
+CHECK_ROOT() {
+
+    if [ $1 -ne 0 ]
+    then
+        echo "Please run the cript with user priveleges"
+
+    fi
+}
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is.... FAILED"
+    else
+        echo "$2 is... SUCCESS"
+}
+
+CHECK_ROOT
+
+for package in $@
+do
+    dnf list installed $package -y
+    VALIDATE $? "Installing $package"
+    dnf install $package -y
+    VALIDATE $? "Installing $package"
+
+done
+
+
+
