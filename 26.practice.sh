737ef52b9cc3d316cb1028b10676a3598c511230
deepakgajula <gajuladeepak17@gmail.com>
2025-02-10 23:52:50 +0530

    git-practice

--- 26.practice.sh
+++ 26.practice.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
-SOURCE_DIR=$1
-DEST_DIR=${2}
-DAYS=${3:-14} #if $3 is empty, default is 14 days.
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-
 R="\e[31m"
 G="\e[32m"
-N="\e[0m"
 Y="\e[33m"
+N="\e[0m"
+
+
+SOURCE_DIR=$1
+DEST_DIR= $2
+DAYS=${3:-14}
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+
+# if [ -d $SOURCE_DIR ]
+# then
+#     echo "$SOURCE_DIR $G Exists $N"
+# else
+#     echo "$SOURCE_DIR $R does not exist $N"
+# fi
+
+# FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+# echo $FILES
+
+# while IFS= read -r file
+# do
+#     echo $file
+#     rm -rf $file
+#     echo "Deleting File: $file"
+
+# done <<< $FILES
 
 USAGE(){
-    echo -e "$R USAGE:: $N sh 19-backup.sh <source> <destination> <days(optional)>"
+    echo -e "$R USAGE:: $N sh.practice <Source dir> <Destination Dir> <days(Optional)>"
 }
-#check the source and destination are provided
 
 if [ $# -lt 2 ]
 then
@@ -22,39 +40,44 @@ then
 fi
 
 if [ ! -d $SOURCE_DIR ]
-then
-    echo "$SOURCE_DIR does not exist...Please check"
+then 
+    echo "$SOURCE_DIR does not exist....Please Check"
+    exit 1
 fi
+
 
 if [ ! -d $DEST_DIR ]
-then
-    echo "$DEST_DIR does not exist...Please check"
+then 
+    echo "$DEST_DIR does not exist....Please Check"
+    exit 1
 fi
 
-FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +$DAYS)
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
+echo $FILES
 
-echo "Files: $FILES"
-
-if [ ! -z $FILES ] #true if FILES is empty, ! nakes it expression false
+if [ -z $FILES ]
 then
-    echo "Files are found"
+    echo "Files are not present greater than $DAYS"
+    exit 1
+
+else
+    echo "Files found"
     ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
     find ${SOURCE_DIR} -name "*.log" -mtime +$DAYS | zip "$ZIP_FILE" -@
 
-    #check if zip file is successfully created or not
-    if [ -f $ZIP_FILE ]
+    if [ -f $ZIP_FILE ] # -f is for files
     then
-        echo "Successfully zippped files older than $DAYS"
+        echo "Successfully zipped files older than $DAYS"
+
         #remove the files after zipping
-        while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
-        do
+        while IFS= read -r file #IFS, internal field separator, empty it will ignore white spaces. -r is for not to ignore special characters like /
+        do 
             echo "Deleting file: $file"
             rm -rf $file
-        done <<< $FILES
+
+        done <<< $FILES 
     else
         echo "Zipping the files is failed"
         exit 1
     fi
-else
-    echo "No files older than $DAYS"
 fi
