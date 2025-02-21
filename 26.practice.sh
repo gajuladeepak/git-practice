c02bbd6f1d410675e2801b2f09d2322cef0ce308
deepakgajula <gajuladeepak17@gmail.com>
2025-02-21 12:25:29 +0530

    git practice

--- 26.practice.sh
+++ 26.practice.sh
@@ -5,10 +5,11 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOG_FOLDER="/var/log/expense-shell"
+LOG_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOG_FOLDER
 
 USERID=$(id -u)
 
