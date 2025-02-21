51b94f0a06a43b4e906d04a556f62b97cb92c2ba
deepakgajula <gajuladeepak17@gmail.com>
2025-02-21 12:56:43 +0530

    git practice

--- 26.practice.sh
+++ 26.practice.sh
@@ -4,7 +4,7 @@
 
 
 
-LOG_FOLDER="var/log/shell-script"
+LOG_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
