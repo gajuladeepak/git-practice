df3027ef33237ebaccf409d8c420eea571526a02
deepakgajula <gajuladeepak17@gmail.com>
2025-02-21 12:39:58 +0530

    git practice

--- 26.practice.sh
+++ 26.practice.sh
@@ -1,52 +1,56 @@
 #!bin/bash
 
+
+
+
+
+LOG_FOLDER="var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP"
+
+
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOG_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-mkdir -p $LOG_FOLDER
-
-USERID=$(id -u)
-
-CHECK_ROOT() {
+ 
+CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo -e "$Y Please Provide Root Previligies $N" | tee -a $LOG_FILE
-        exit 1
+        echo -e "$Y PLease provide root previliges $N" | tee -a $LOG_FILE
+
     fi
+
 }
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is....... $R FAILURE $N" | tee -a $LOG_FILE
+        echo -e "$2 command is..... $R FAILED $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "$2 is........ $G SUCCESS $N" | tee -a $LOG_FILE
+        echo -e "$2 command is..... $R SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
 
 CHECK_ROOT
 
-dnf list installed mysql &>>$LOG_FILE
+for package in $@
+do
+    dnf list installed $package
+    if [ $? -ne 0 ]
+        echo -e "$package is not installled.. $Y INSTALLING $N" | tee -a $LOG_FILE
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "INSTALLING $package"
 
-if [ $? -ne 0 ]
-then
-    echo "mysql is not installed... Installing it"
-    dnf install mysql -y &>>$LOG_FILE
-    VALIDATE $? "Installing Mysql"
-else
-    echo "Git is already installed" | tee -a $LOG_FILE
-fi
+    then
+    fi
 
-
-    
+done
 
 
 
