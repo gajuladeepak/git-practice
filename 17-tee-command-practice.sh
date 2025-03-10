e8f3528e1c6dcdc0032bc22d9010b8730a18a4b0
deepakgajula <gajuladeepak17@gmail.com>
2025-03-10 21:54:20 +0530

    git--practice

--- 17-tee-command-practice.sh
+++ 17-tee-command-practice.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-LOGs_FOLDER="/var/log/shell-script"
+LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE="$LOGs_FOLDER/$SCRIPT_NAME-$TIMESTAMP"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
 
 USERID=$(id -u)
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo "$2 is... SUCCESS"
-    else
-        echo "$2 is... FAILURE"
-    fi
-}
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
 
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
-        echo "Please provide root preveliges"
+    then
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
 USAGE(){
-    echo "USAGE:: sudo sh 16-redirectors.sh package1 package2.."
+    echo -e "$R USAGE:: $N sudo sh 16-redirectors.sh package1 package2 ..."
     exit 1
 }
+
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
 
@@ -40,15 +44,15 @@ then
     USAGE
 fi
 
-for package in $@
+for package in $@ # $@ refers to all arguments passed to it
 do
-    dnf list installed $package -y
-    VALIDATE $? "list installed"
+    dnf list installed $package &>>$LOG_FILE
     if [ $? -ne 0 ]
     then
-        echo "$package is not installed, going to install it..."
-        dnf install $package -y
-        VALIDATE $? "installing $package"
-
+        echo "$package is not installed, going to install it.." | tee -a $LOG_FILE
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "Installing $package"
+    else
+        echo -e "$package is already $Y installed..nothing to do $N" | tee -a $LOG_FILE
+    fi
 done
-
--- 23-practice.sh
+++ 23-practice.sh
@@ -1,65 +1,32 @@
-#!/bin/bash
+#!bin/bash
 
+SOURCE_DIR="/home/ec2-user/logs"
 
-LOGS_FOLDER="var/log/shell-script"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOGFILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-mkdir -p $LOGS_FOLDER
-
-
-
-USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-CHECK_ROOT() {
-
-    if [ $USERID -ne 0 ]
-    then
-        echo -e "$R Please run the cript with user priveleges $N" | tee -a $LOGFILE
-
-    fi
-}
-
-USAGE(){
-    echo -e "$R USAGE:: $N Expected Inputs sudo sh 16-redirectors.sh package1 package2.."
-}
-
-
-
-VALIDATE() {
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 is.... $R FAILED $N" | tee -a $LOGFILE
-    else
-        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOGFILE
-    fi
-}
-
-echo "Script started executing at: $(date)" &>>LOGFILE | tee -a $LOGFILE
-CHECK_ROOT
-if [ $# -eq 0 ]
+if [ -d $SOURCE_DIR ]
 then
-    USAGE
+    echo -e "$G Folder EXISTS $N"
+else
+    echo -e "$R Folder is not found $N"
+    exit 1
+
 fi
 
-for package in $@
+FILES=$(find $SOURCE_DIR -name "*log" -mtime +14)
+echo $FILES
+
+
+while IFS= read -r file
 do
-    dnf list installed $package -y &>>$LOGFILE
-    if [ $? -ne 0]
-    then 
-        echo echo "$package is not installed, going to install it.."| tee -a $LOGFILE
-        dnf install $package -y
-        VALIDATE $? "Installing $package"
-    else
-         echo -e "$package is already $Y installed..nothing to do $N" | tee -a $LOGFILE
-
-    fi
-
-done
+    echo "Deleting the: $file"
+    rm -rf $file
+    echo "Deleted the $file"
 
 
+
+done <<< $FILES
 
